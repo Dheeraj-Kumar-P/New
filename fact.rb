6e4b4b439d936a2864b607ad445b02fd407539f1
Dheeraj <dheeraj@qburst.com>
2016-06-23 17:51:45 +0530

    Factorial

---
+++ fact.rb
@@ -0,0 +1,16 @@
+def fact(num)
+if num==0
+return 1	
+end
+if num>0
+return (1..num).reduce(:*)
+else 
+print "Factorial not possible"
+exit
+end
+end
+print "Enter the number: "
+no=gets.to_i
+a=fact(no)
+puts "Factorial: #{a}"
+
