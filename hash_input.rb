d6fde28fc469164586eec53e1097c078dbc681ee
Dheeraj <dheeraj@qburst.com>
2016-06-24 14:56:03 +0530

    Hash

---
+++ hash_input.rb
@@ -0,0 +1,18 @@
+hash={}
+key=[]
+key=gets.chomp
+key=key.split(' ')
+value=[]
+value=gets.chomp
+value=value.split(' ')
+index=0
+key.each do |ki|
+val=value[index]
+value[index]=value[index].to_i if val.to_i!=0
+k=ki
+ki=ki.to_i if k.to_i!=0
+hash[ki]=value[index]
+index+=1
+end
+print hash
+
